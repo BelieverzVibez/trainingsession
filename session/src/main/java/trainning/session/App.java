4c74ae5551be8db5918d52961475608785826ce2
selva <selva@DESKTOP-QGIA4I3>
2020-04-09 11:21:00 +0530

    First commit into the repository

---
+++ session/src/main/java/trainning/session/App.java
@@ -0,0 +1,13 @@
+package trainning.session;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
